bd41ac1a2a525acfede1dad38bfcc5a9e25ed394
Kuffel <adam@kuffel.eu>
2020-09-22 13:34:29 +0200

    Added credo for static code analysis

--- lib/ex_app_web/telemetry.ex
+++ lib/ex_app_web/telemetry.ex
@@ -1,4 +1,6 @@
 defmodule ExAppWeb.Telemetry do
+  @moduledoc false
+
   use Supervisor
   import Telemetry.Metrics
 
--- mix.exs
+++ mix.exs
@@ -41,7 +41,10 @@ defmodule ExApp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      # Custom dependencies
+      {:credo, "~> 1.1", runtime: false}
     ]
   end
 
