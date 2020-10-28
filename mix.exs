be738725e2fdc6bfbef6acbef6704aeee46baa79
Kuffel <adam@kuffel.eu>
2020-10-28 21:19:31 +0100

    Enabled phoenix live dashboard and os monitoring for production builds

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule ExApp.MixProject do
   def application do
     [
       mod: {ExApp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
