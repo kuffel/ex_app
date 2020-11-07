a556456bd7b05815ae6708e471d275fa48d21fb4
Kuffel <adam@kuffel.eu>
2020-11-07 14:59:04 +0100

    Added cleanup step after production deployment that removes unused images

--- deployments/scripts/get_pull_requests.sh
+++ deployments/scripts/get_pull_requests.sh
@@ -3,8 +3,7 @@ set -e
 # Returns information about all pull requests that are available in the GitHub Repo.
 
 GITHUB_TOKEN=${1:-`cat ~/.github_token`}
-# LAMBDA_URL=${2:-"https://cn6abx7xxi.execute-api.eu-central-1.amazonaws.com/api"}
-LAMBDA_URL=${2:-"http://localhost:8000"}
+LAMBDA_URL=${2:-"https://cn6abx7xxi.execute-api.eu-central-1.amazonaws.com/api"}
 
 wget --quiet \
 --method GET \
